d89ebe91fa620a0da6f2f11285d89d117a6b91e7
lignitovivo <adrianvillazon@gmail.com>
2023-06-08 09:12:43 -0500

    Creaccion de la Clase Pizza

---
+++ src/modelo/Pizza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author adria
+ */
+public class Pizza {
+    String sabor;
+    String tamaño;
+    int cantidad;
+    String estado;
+}
