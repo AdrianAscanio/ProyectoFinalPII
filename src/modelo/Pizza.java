9e0a248427205c5da4d4950b3667d7b76a98c6b4
lignitovivo <adrianvillazon@gmail.com>
2023-06-09 10:03:38 -0500

    Finalizacion  POJO jAVA

--- src/modelo/Pizza.java
+++ src/modelo/Pizza.java
@@ -9,8 +9,58 @@ package modelo;
  * @author adria
  */
 public class Pizza {
-    String sabor;
-    String tamaño;
-    int cantidad;
-    String estado;
+    private String sabor;
+    private String tamaño;
+    private int cantidad;
+    private String estado;
+
+    public Pizza() {
+        this.sabor = "";
+        this.tamaño ="";
+        this.cantidad = 0;
+        this.estado = "";
+    }
+
+    public Pizza(String sabor, String tamaño, int cantidad, String estado) {
+        this.sabor = sabor;
+        this.tamaño = tamaño;
+        this.cantidad = cantidad;
+        this.estado = estado;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
 }
