30021d011d2a80473469b3a068a82fb8571f0f35
lignitovivo <adrianvillazon@gmail.com>
2023-06-09 10:43:23 -0500

    agregarcion de metodo AddPizza en PizzaPila

--- src/modelo/PizzaPila.java
+++ src/modelo/PizzaPila.java
@@ -9,6 +9,51 @@ package modelo;
  * @author adria
  */
 public class PizzaPila extends Pizza {
-    PizzaPila cab;
+
+    private PizzaNodo cab;
+    private int nPizzas;
+
+    public PizzaPila() {
+    }
+
+    public PizzaPila(String sabor, String tamaño, int cantidad, String estado) {
+        super(sabor, tamaño, cantidad, estado);
+    }
+
+    public PizzaNodo getCab() {
+        return cab;
+    }
+
+    public void setCab(PizzaNodo cab) {
+        this.cab = cab;
+    }
+
+    public int getnPizzas() {
+        return nPizzas;
+    }
+
+    public void setnPizzas(int nPizzas) {
+        this.nPizzas = nPizzas;
+    }
+
+    public boolean pilaVacia() {
+        if (this.nPizzas == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addPizza(String Id,String sabor, String tamaño, int cantidad, String estado) {
+        PizzaNodo n = new PizzaNodo(Id,sabor.toUpperCase(), tamaño.toUpperCase(), cantidad, estado.toLowerCase());
+        if (this.pilaVacia() == true) {
+            this.setCab(n);
+        } else {
+            PizzaNodo temp = this.getCab();
+            n.setSig(temp);
+            this.setCab(n);
+        }
+    }
     
+
 }
