22261b593d8b2527b590495f6abd4bb8305311e4
lignitovivo <adrianvillazon@gmail.com>
2023-06-20 10:04:03 -0500

    Actualizacion PopMenuController -> 20/06/23

--- src/buonazza/PopMenuController.java
+++ src/buonazza/PopMenuController.java
@@ -4,9 +4,16 @@
  */
 package buonazza;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -18,12 +25,23 @@ public class PopMenuController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    public void  popUp(){
+    @FXML
+    public Button btnClose;
+    public void  popUp() throws IOException{
+        Stage stageEme=new Stage();
+        Parent h = FXMLLoader.load(getClass().getResource("popMenu.fxml"));
+        Scene canva = new Scene(h);
+        stageEme.setScene(canva);
+        stageEme.showAndWait();
+    }
+    public void popClose(){
+        Stage res = (Stage) btnClose.getScene().getWindow();
+        res.close();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
